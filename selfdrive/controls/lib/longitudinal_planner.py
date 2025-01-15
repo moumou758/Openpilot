0bdf444542c429f643c93d6c59623a092b506d17
moumou758 <moumou758@163.com>
2025-01-15 13:01:47 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,8 +20,8 @@ from openpilot.sunnypilot.selfdrive.controls.lib.dec.helpers import DecPlanner
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
+A_CRUISE_MAX_VALS = [1.4, 1.2, 0.9, 0.86, 0.68, .45,  .32,  .28,  .088]
+A_CRUISE_MAX_BP =   [0.,  1.,  6.,  8.,   11.,  20.,  25.,  30.,  55.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.5
 MIN_ALLOW_THROTTLE_SPEED = 2.5
